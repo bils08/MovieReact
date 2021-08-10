8f133fca60928bbe08797481943cf0e2c732f001
Glitch (starter-cra-and-express) <none>
2021-08-10 03:53:13 +0000

    🚠🎿 Checkpoint ./src/App.js:34742488/1149 ./src/CardList.js:34742488/737 ./src/MovieList.js:34742488/319

---
+++ src/MovieList.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const MovieList = (props) => {
+  return
+  (
+    <div>
+      {props.movies.map((movie, index) => {
+        <div>
+          <h1>{movie.</h1>
+        </div>
+      })}
+    </div>
+  )
+}
