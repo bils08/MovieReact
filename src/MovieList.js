398cf3e035473d404933a5c4901b887871ce945b
Glitch (starter-cra-and-express) <none>
2021-08-10 12:21:52 +0700

    MovieReact add file

--- src/MovieList.js
+++ src/MovieList.js
@@ -20,7 +20,7 @@ function MovieList() {
   (
     <div className="row">
       {props.movies.slice(0, 4).map((movie, index) => {
-        <div className="col-3" key={movie.id}>
+        <div className="col-md-3" key={movie.id}>
           <h1>{movie.original_title}</h1>
           <h3>{movie.tagline}</h3>
           <h3>{movie.vote_average}</h3>
